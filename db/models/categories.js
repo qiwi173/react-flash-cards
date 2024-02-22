60919aaccd4f723c4b0183761b58df9f98b99c33
decoy1337 <leilichjaysin@rambler.ru>
2024-02-22 20:02:16 +0300

    db

---
+++ db/models/categories.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model, HasMany
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class categories extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({Quesion}) {
+      this.hasMany(Quesion,{foreignKey:'categoriesid'})
+    }
+  }
+  categories.init({
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT
+      },
+      img: {
+        allowNull: false,
+        type: DataTypes.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+  }, {
+    sequelize,
+    modelName: 'categories',
+  });
+  return categories;
+};
